41c92dcbbdf002c06c06906e48aabcd7c6f16331
dumkalpoly <dumkalpoly>
2021-05-22 17:34:48 +0530

    Add Routine

---
+++ routine/precache-manifest.af6693e478a81dc85e18f01795e85d69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f13236a1b15641dc44ba0f24ce3a5e0",
+    "url": "/3rdYear2021/routine/index.html"
+  },
+  {
+    "revision": "646b216034db108753e9",
+    "url": "/3rdYear2021/routine/static/css/main.86e77ec8.chunk.css"
+  },
+  {
+    "revision": "2a4a468d549b13e61154",
+    "url": "/3rdYear2021/routine/static/js/2.31dc192d.chunk.js"
+  },
+  {
+    "revision": "596e4823497e45004d2db4c50beba3f3",
+    "url": "/3rdYear2021/routine/static/js/2.31dc192d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "646b216034db108753e9",
+    "url": "/3rdYear2021/routine/static/js/main.f2f1c567.chunk.js"
+  },
+  {
+    "revision": "7ec8f52f5b5ccb283bb1",
+    "url": "/3rdYear2021/routine/static/js/runtime-main.735fc717.js"
+  }
+]);
